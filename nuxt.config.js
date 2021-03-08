651c2fcbde12f8159226a735968b2db576566bc7
rath_miguel <rath.miguel@gmail.com>
2021-03-08 12:07:14 +0900

    タイトルの追加

--- nuxt.config.js
+++ nuxt.config.js
@@ -29,7 +29,8 @@ export default {
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
     '~plugins/vue-scrollto',
-    '~plugins/vee-validate'
+    '~plugins/vee-validate',
+    { src: '~plugins/ga.js', mode: 'client' }
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
